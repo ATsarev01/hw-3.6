af94f049751395526bcf640ca67fb8f633c7e866
ATsarev01 <maxperov01@gmail.com>
2023-05-28 17:59:10 +0600

    initial commit

---
+++ src/main/java/ru/hogwarts/schoolfinal/SchoolFinalApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.schoolfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolFinalApplication.class, args);
+    }
+
+}
---
+++ src/main/java/ru/hogwarts/schoolfinal/repository/FacultyRepository.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.schoolfinal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.hogwarts.schoolfinal.entity.Faculty;
+
+import java.util.List;
+
+public interface FacultyRepository extends JpaRepository<Faculty, Long> {
+
+    List<Faculty> findAllByColor(String color);
+    List<Faculty> findAllByNameContainsIgnoreCaseOrColorContainsIgnoreCase(String name, String color);
+
+}
